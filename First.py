9823e2d0be503fcb92b45d45192932516996bbf7
Ameen <ameen.ahmed@tdh.ch>
2023-12-24 19:33:53 +0200

    adding Testing file

---
+++ First.py
@@ -0,0 +1,16 @@
+mystring = "   i love 1996 dsoooo     "
+print(mystring.lstrip())
+print(mystring.capitalize())
+
+a , b , c , d  = "1" , "11" , "111" , "1111"
+print(a.zfill(2))
+print(b.zfill(2))
+print(c.zfill(2))
+print(d.zfill(2))
+
+ar = "Ameen"
+print(ar.upper())
+print(ar.lower())
+
+am = "ameen will go to the gym"
+print(len(am))
